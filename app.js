32dfdb586ba647d8e1c134f11d7eba2da3e2ee34
ashelake <ashelake88@gmail.com>
2022-11-07 00:09:26 +0530

    CRUD Backend

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("./db/conn");
+const users = require("./models/userSchema");
+const cors = require("cors");
+const router = require("./routes/router");
+const { connection } = require("./db/conn");
+
+const port = process.env.PORT || 8003;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("server start");
+});
+
+app.use(router);
+
+app.listen(port, async () => {
+  try {
+    await connection;
+    console.log("Listining to port");
+  } catch (err) {
+    console.log("Error in Connectiong DB");
+    console.log(err);
+  }
+  console.log(`server is start port number ${port}`);
+});
